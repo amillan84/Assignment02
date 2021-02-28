e4e503ebc67c7ce3ad4af1b73425c22adb7ffb8d
Aida Millan <amillan84@gmail.com>
2021-02-27 18:13:07 -0800

    changes author label comment first file

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -1,5 +1,6 @@
-//App Name: My second assignment
-//Author: Aida Millan~Rocha
+/*App Name: My second assignment
+Author: Aida Millan~Rocha
+Date: Feb 26,2021*/
 
 //  let x = parseInt(prompt("Guess a number between 1 to 50 to see if x is bigger than y"));
 //  let y = 20;
